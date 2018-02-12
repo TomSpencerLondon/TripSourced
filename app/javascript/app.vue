8d8159779d9887726178e718732a35554373423f
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-02-12 16:53:49 +0000

    Change app.vue file for vue.js cards

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app" class="row">
-    <div v-for="list in original_lists" class="col-3">
+    <div v-for="(list, index) in original_lists" class="col-3">
       <h6>{{ list.name }}</h6>
       <hr />
       <div v-for="(card, index) in list.cards" class="card card-body">
       {{ card.name }}
+      </div>
+      <div class="card card-body">
+      <textarea v-model="messages[list.id]" class="form-control"></textarea>
+      <button class="btn btn-secondary">Add</button>
       </div>
     </div>
   </div>
@@ -13,6 +17,13 @@
 <script>
 export default {
   props: ["original_lists"],
+  data: function() {
+    return {
+      messages: {
+        0: "SDFSDF", 
+        1: "asdfljkasdf"}
+    }
+  }
 }
 </script>
 
